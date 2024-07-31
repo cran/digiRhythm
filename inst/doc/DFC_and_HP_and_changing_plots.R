4b324dd2cbc2d6038083b5d3c6286bb4cfa97473
Hassan-Roland Nasser <hassan.nasser@me.com>
2024-07-31 09:30:02 +0000

    version 2.2

--- inst/doc/DFC_and_HP_and_changing_plots.R
+++ inst/doc/DFC_and_HP_and_changing_plots.R
@@ -17,8 +17,16 @@ df <- data[1:672, c("datetime", activity)]
 my_lsp <- lomb_scargle_periodogram(df, alpha = 0.01, plot = TRUE)
 
 ## ----dfc_hp-------------------------------------------------------------------
-my_dfc <- dfc(data, activity = activity, plot = FALSE, verbose = FALSE)
-# Setting plot to TRUE will visualize all the LSP plots (like the one above) during each sliding window. Rather used for deeper investigation and debugging.
+my_dfc <- dfc(data, # The dataset
+  activity = activity, # the name of the activity column
+  sampling = 15, # the sampling frequency of my dataset is 15
+  alpha = 0, 5, # significance level
+  harm_cutoff = 12, # up till the harmonic 24/12 which is two hours
+  plot = FALSE, verbose = FALSE
+)
+# Setting plot to TRUE will visualize all the LSP plots (like the one above)
+# during each sliding window. Rather used for deeper investigation and
+# debugging.
 print(my_dfc)
 
 ## ----type_dfc-----------------------------------------------------------------
@@ -34,7 +42,7 @@ print(my_dfc)
 library(ggplot2)
 new_plot <- my_dfc +
   theme(
-    text = element_text(family = "Times", size = 8),
+    text = element_text(family = "Times", size = 14),
     axis.text.y = element_text(size = 15, colour = "black")
   )
 print(new_plot)
